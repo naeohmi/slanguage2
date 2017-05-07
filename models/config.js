49bd6da596b7f839703a6f0df841f7595697f50c
naeohmi <naeohmi@gmail.com>
2017-05-07 15:53:13 -0400

    heroku db

--- models/config.js
+++ models/config.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 const promise = require('bluebird');
 const options = { promiseLib: promise };
 const pgp = require('pg-promise')(options)
-const connectionString = 'postgres://localhost:5432/slanguage2';
-const db = pgp(connectionString);
+const connectionString = 'postgres://naeohmi@localhost:5432/slanguage2';
+const db = pgp(connectionString || process.env.DATABASE_URL);
 
 module.exports = {
     db: db,
