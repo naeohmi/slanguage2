ebaa1b17c52e93e28c25da46e707b6876fab6158
naeohmi <naeohmi@gmail.com>
2017-05-07 17:04:58 -0400

    destroy

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,21 +3,28 @@ console.log('main.js is working')
 $('.edit').on('click', () => {
     $(this).prev().focus();
 });
+
+$('.destroy').on('click', () => {
+    let id = $(this).parent().attr('data-id')
+    axios.delete("https://slanguage2.herokuapp.com/words/" + id)
+    $(this).parent().remove();
+    location.reload();
+});
+
 $('.input_item').on('change', () => {
     let id = parseInt($(this).parent().attr('data-id'))
     let val = $(this).val();
+
     axios.patch("https://slanguage2.herokuapp.com/words/" + id, {
-        val: val,
+        post: val,
         id: id
     }).catch((err) => {
         return next(err);
     });
+    location.reload();
 });
-// $('.destroy').on('click', () => {
-//     let id = $(this).parent().attr('data-id')
-//     axios.delete("http://localhost:3000/" + words + "/" + id)
-//     $(this).parent().remove();
-// });
+
+
 const destroy = document.querySelectorAll('.destroy');
 destroy.forEach((el, index) => {
     el.addEventListener('click', () => {
