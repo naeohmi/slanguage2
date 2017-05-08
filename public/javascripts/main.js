61fb12bcf2d9855f0b506bc7adba13d8b24391a7
naeohmi <naeohmi@gmail.com>
2017-05-08 09:20:04 -0400

    destroy edits

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,12 +2,12 @@ $('.edit').on('click', () => {
     $(this).prev().focus();
 });
 
-$('.destroy').on('click', () => {
-    let id = $(this).parent().attr('data-id')
-    axios.delete("https://slanguage2.herokuapp.com/words/" + id)
-    $(this).parent().remove();
-    // location.reload();
-});
+// $('.destroy').on('click', () => {
+//     let id = $(this).parent().attr('data-id')
+//     axios.delete("https://slanguage2.herokuapp.com/words/" + id)
+//     $(this).parent().remove();
+//     // location.reload();
+// });
 
 $('.input_item').on('change', () => {
     let id = parseInt($(this).parent().attr('data-id'))
@@ -23,11 +23,17 @@ $('.input_item').on('change', () => {
 });
 
 
-const destroy = document.querySelectorAll('.destroy');
-destroy.forEach((el, index) => {
-    el.addEventListener('click', () => {
-        let id = el.getAttribute('id');
-        el.parentNode.removeChild(el.parentNode);
+const destroy = document.querySelector('.destroy');
+destroy.addEventListener('click', () => {
+        destroy.setAttribute('class', 'clicked');
+        destroy.parentNode.innerHTML = '';
+
+
+        // const destroy = document.querySelectorAll('.destroy');
+        // destroy.forEach((el, index) => {
+        // el.addEventListener('click', () => {
+        // let id = el.getAttribute('id');
+        // el.parentNode.removeChild(el.parentNode);
         axios.delete("https://slanguage2.herokuapp.com/words/" + id)
     })
-});
+    // });
